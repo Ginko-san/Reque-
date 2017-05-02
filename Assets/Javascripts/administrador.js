4af72ba482ed4ae1001cd6f868de5ea94effe632
GabrielPerezB <gabrielapb1996@gmail.com>
2017-05-01 22:35:18 -0600

    soy noob

--- Assets/Javascripts/administrador.js
+++ Assets/Javascripts/administrador.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
-    $(".dropdown-button").dropdown({
-        hover: false
-    });
-        
-});
+var miAplicacion = angular.module('admin', [])
+
+miAplicacion.controller('controlador1',['$scope', function($scope){
+    $scope.habitaciones=[
+            {numero: "1" , descripcion: "Ocupada"},
+            {numero: "2" , descripcion: "Desocupada"},
+            {numero: "3" , descripcion: "Desocupada"},
+            {numero: "4" , descripcion: "Ocupada"},
+            {numero: "5" , descripcion: "Ocupada"},
+            {numero: "6" , descripcion: "Desocupada"},
+            {numero: "7" , descripcion: "Ocupada"},
+            {numero: "8" , descripcion: "Desocupada"},
+            {numero: "9" , descripcion: "Ocupada"},
+    ];
+}])
